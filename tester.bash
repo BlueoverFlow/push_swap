0bf1ce58b444efea92fa7580b865ae8f984ddd91
Ael-mezz <achraf.elmezzioui@gmail.com>
2021-10-28 11:41:38 +0100

    Update tester.bash

--- tester.bash
+++ tester.bash
@@ -1,364 +1,395 @@
-# **************************************************************************** #
-#                                                                              #
-#                                                         :::      ::::::::    #
-#    tester.sh                                          :+:      :+:    :+:    #
-#                                                     +:+ +:+         +:+      #
-#    By: ael-mezz <ael-mezz@student.1337.ma>        +#+  +:+       +#+         #
-#                                                 +#+#+#+#+#+   +#+            #
-#    Created: 2021/03/21 12:18:52 by ael-mezz          #+#    #+#              #
-#    Updated: 2021/05/12 15:36:15 by ael-mezz         ###   ########.fr        #
-#                                                                              #
-# **************************************************************************** #
 
-#!/bin/bash
+#include "get_next_line.h"
 
-red=$'\e[1;31m'
-grn=$'\e[1;32m'
-yel=$'\e[1;33m'
-blu=$'\e[1;34m'
-mag=$'\e[1;35m'
-cyn=$'\e[1;36m'
-end=$'\e[0m'
-ERR=0
-WRC=0
+int ft_strlen(char *str)
+{
+    int i;
 
-printf "\e[30;103mTesting checker with non numeric parameters${end}" ############################
-RET=`echo rr | ./checker 1 -548368 2478 6p98 .48 ,7`
-		if [ "$RET" != "Error!" ];
-			then
-			printf "\t${red}✘${end}" 
-		else
-			printf "\t${grn}√${end}" 
-        fi
-RET=`echo rr | ./checker mate8823 99`
-		if [ "$RET" != "Error!" ];
-			then
-			printf "\t${red}✘${end}\n" 
-		else
-			printf "\t${grn}√${end}\n" 
-        fi
+    i = 0;
+    if (str)
+        while (str[i])
+            i++;
+    return (i);
+}
 
-printf "\n\e[30;103mTesting checker with duplicate parameter${end}" ############################
-ARG=`ruby -e "puts (-500..500).to_a.shuffle.join(' ')"`
-RET=`echo rr | ./checker 250 $ARG`
-		if [ "$RET" != "Error!" ];
-			then
-			printf "\t${red}✘${end}\n" 
-		else
-			printf "\t${grn}√${end}\n" 
-        fi
-		
-printf "\n\e[30;103mTesting checker with MAXINT parameter${end}\t" ############################
-ARG=`ruby -e "puts (-500..500).to_a.shuffle.join(' ')"`
-RET=`echo rr | ./checker $ARG 2147483647 -2147483648`
-		if [ "$RET" == "Error!" ];
-			then
-			printf "\t${red}✘${end}" 
-		else
-			printf "\t${grn}√${end}" 
-        fi
-ARG=`ruby -e "puts (-500..500).to_a.shuffle.join(' ')"`
-RET=`echo rr | ./checker $ARG 2147483648 -2147483649`
-		if [ "$RET" != "Error!" ];
-			then
-			printf "\t${red}✘${end}\n" 
-		else
-			printf "\t${grn}√${end}\n" 
-        fi
+char    *push_line(char *remains)
+{
+    int i;
+    char *array;
 
-printf "\n\e[30;103mTesting checker without any parameter${end}\t" ############################
-RET=`echo "" | ./checker  `
-		if [ "$RET" != "" ];
-			then
-			printf "\t${red}✘${end}\n" 
-		else
-			printf "\t${grn}√${end}\n" 
-        fi
+    i = 0;
+    while (remains[i] && remains[i] != '\n')
+        i++;
+    if (!(array = (char *)malloc(sizeof(char) * (i + 1))))
+        return (NULL);
+    i = 0;
+    while (remains[i] && remains[i] != '\n')
+    {
+        array[i] = remains[i];
+        i++;
+    }
+    array[i] = '\0';
+    return (array);
+}
 
-printf "\n\e[30;103mTesting checker with unvalid action${end}\t" #############################
-ARG=`ruby -e "puts (-500..500).to_a.shuffle.join(' ')"`
-RET=`echo rm | ./checker $ARG`
-		if [ "$RET" != "Error!" ];
-			then
-			printf "\t${red}✘${end}" 
-		else
-			printf "\t${grn}√${end}" 
-        fi
-RET=`echo rr | ./checker $ARG`
-		if [ "$RET" == "Error!" ];
-			then
-			printf "\t${red}✘${end}" 
-		else
-			printf "\t${grn}√${end}" 
-        fi
-RET=`echo "rr   " | ./checker $ARG`
-		if [ "$RET" != "Error!" ];
-			then
-			printf "\t${red}✘${end}" 
-		else
-			printf "\t${grn}√${end}" 
-        fi
-RET=`echo "    rr" | ./checker $ARG`
-		if [ "$RET" != "Error!" ];
-			then
-			printf "\t${red}✘${end}\n" 
-		else
-			printf "\t${grn}√${end}\n" 
-        fi
+char    *cut_next_line(char *remains)
+{
+    int i;
+    int j;
+    char *array;
 
-printf "\n\t\e[30;103mTesting with 2 params${end}\n" ##############################
-for i in range {1..10}
-	do 
-		ARG=`ruby -e "puts (-1..0).to_a.shuffle.join(' ')"`
-		RET=`./push_swap $ARG | ./checker $ARG`
-        ACT=`./push_swap $ARG | wc -l`
-		if [ "$RET" != "OK" ];
-			then
-			((ERR++))
-			printf "${red}▓${end}" 
-		else
-			printf "${grn}▓${end}" 
-        fi
-        if [ $ACT -gt 12 ];
-			then
-			((ERR++))
-			printf "${red}▓${end}" 
-		else
-			printf "${grn}▓${end}"
-		fi
-        if [ $ACT -gt $WRC ];
-            then
-            WRC=$ACT
-        fi
-        printf " actions: %-4i" $ACT
-        echo ' input: ' $ARG
-done
-printf "\e[30;103m\tWorst case found: %i ${end}" $WRC
+    i = 0;
+    j = 0;
+    while (remains[i] && remains[i] != '\n')
+        i++;
+    if (!remains[i])
+    {
+        free(remains);
+        return (NULL);
+    }
+    if (!(array = (char *)malloc(sizeof(char) * (ft_strlen(remains) - i + 1))))
+        return (NULL);
+    i++;
+    while (remains[i])
+    {
+        array[j] = remains[i];
+        i++;
+        j++;
+    }
+    array[j] = '\0';
+    free(remains);
+    return (array);
+}
 
-if [ $ERR -eq 0 ];
-	then
-	printf "${grn}\tSUCCESS [200/200]${end}\n" $ERR
-else
-	printf "${red}\tFAIL[%i/200] 😱 😱 😱 ${end}\n" $ERR
-fi
+char *ft_strjoin(char *remains, char *buffer)
+{
+    int i;
+    int j;
+    char *new;
 
-printf "\n\t\e[30;103mTesting with 3 params${end}\n" ##############################
-for i in range {1..20}
-	do 
-		ARG=`ruby -e "puts (0..2).to_a.shuffle.join(' ')"`
-		RET=`./push_swap $ARG | ./checker $ARG`
-        ACT=`./push_swap $ARG | wc -l`
-		if [ "$RET" != "OK" ];
-			then
-			((ERR++))
-			printf "${red}▓${end}" 
+    if (!remains && !buffer)
+        return (NULL);
+    new = malloc(sizeof(char) * (ft_strlen(remains) + ft_strlen(buffer) + 1));
+    i = -1;
+    j = 0;
+    if (remains)\
+    
+        while (remains[++i])
+            new[j++] = remains[i];
+    i = -1;
+    j--;
+    while (buffer[++i])
+        new[j++] = buffer[i];
+    new[j] = '\0';
+    free((void *)remains);
+    return (new);
+}
+
+int find_char(char *str, char c)
+{
+	int i = -1;
+
+	if (str)
+		while (str[++i])
+			if (str[i] == c)
+				return (1);
+	return (0);
+}
+
+int get_next_line(int fd, char **line)
+{
+    char buffer[BUFFER_SIZE + 1];
+    static char *remains;
+    int count;
+
+    count = 1;
+    if (!line)
+        return (-1);
+    buffer[0] = '\0';
+    while (find_char(buffer, '\n') && count)
+    {
+        if ((count = read(fd, buffer, BUFFER_SIZE)) == -1)
+            return (-1);
+        buffer[count] = '\0';
+        remains = ft_strjoin(remains, buffer);
+    }
+    *line = push_line(remains);
+    remains = cut_next_line(remains);
+    return (count == 0 ? 0 : 1);
+}
+
+#include <stdarg.h>
+#include <stdlib.h>
+#include <unistd.h>
+
+typedef struct s_list
+{
+	int width;
+	int point;
+	int precis;
+} t_list;
+
+t_list init_struct(t_list flags)
+{
+	flags.width = 0;
+	flags.point = 0;
+	flags.precis = 0;
+	return (flags);
+}
+
+int ft_isdigit(char c)
+{
+	return (c >= '0' && c <= '9');
+}
+
+int is_spec(char c)
+{
+	return (c == 's' || c == 'd' || c == 'x');
+}
+
+t_list pars_flags(t_list flags, char *str)
+{
+	int i = 0;
+
+	while (str[i] && (is_spec(str[i]) == 0))
+	{
+		if (ft_isdigit(str[i]) && flags.point == 0)
+			flags.width = (flags.width * 10) + (str[i] - '0');
+		else if (str[i] == '.' && flags.point == 0)
+			flags.point = 1;
+		else if (ft_isdigit(str[i]) && flags.point == 1)
+			flags.precis = (flags.precis * 10) + (str[i] - '0');
+		i++;
+	}
+	return (flags);
+}
+
+int check_size(int number)
+{
+	unsigned int num;
+	int count = 0;
+
+	if (number == 0)
+		return (1);
+	if (number < 0)
+	{
+		count++;
+		num = number * (-1);
+	}
+	else
+		num = number;
+	while (num > 0)
+	{
+		count++;
+		num /= 10;
+	}
+	return (count);
+}
+
+int ft_strlen (char *str)
+{
+	int i = 0;
+
+	while (str[i])
+		i++;
+	return (i);
+}
+
+char *get_num(int number, int size)
+{
+	unsigned num;
+	char *str;
+
+	str = (char *)malloc(sizeof(char) * (size + 1));
+	str[size] = '\0';
+	if (number == 0)
+	{
+		str[0] = '0';
+		return (str);
+	}
+	if (number < 0)
+		num = number * (-1);
+	else
+		num = number;
+	while (--size > 0)
+	{
+		str[size] = (num % 10) + '0';
+		num /= 10;
+	}
+	if (number < 0)
+		str[size] = '-';
+	else
+		str[size] = (num % 10) + '0';
+	return (str);
+}
+
+int print_width(int size, char c)
+{
+	int count = 0;
+
+	while (count < size)
+	{
+		write(1, &c, 1);
+		count++;
+	}
+	return (count);
+}
+
+int ft_putstr(char *str, int size)
+{
+ 	    int count = 0;
+ 
+ 	    while (count < size)
+	    {
+	        write(1, &str[count], 1);
+	        count++;
+	    }
+	 return (count);
+}
+
+int print_int(t_list flags, va_list value)
+{
+	int count = 0;
+	int number;
+	char *str;
+	int size;
+
+	number = va_arg(value, int);
+	size = check_size(number);
+	str = get_num(number, size);
+	if (flags.precis == 0 && flags.point == 1 && number == 0)
+	{
+		free(str);
+		str = "";
+	}
+	size = ft_strlen(str);
+	if (number < 0)
+		size--;
+	count += print_width(flags.width - ((flags.precis > size ? ((number < 0) ? (flags.precis + 1) : flags.precis) : ((number < 0) ? (size + 1) : size))), ' ');
+	if (number < 0)
+		count += print_width(1, '-');
+	count += print_width(flags.precis - size, '0');
+	count += ft_putstr(((number < 0) ? &str[1] : &str[0]), size);
+	if (!(flags.precis == 0 && flags.point == 1 && number == 0))
+		free(str);
+	return (count);
+}
+
+int print_str(t_list flags, va_list value)
+{
+	char *str;
+	int size;
+	int count = 0;
+
+	str = va_arg(value, char *);
+	if (!str)
+		str = "(null)";
+	if (flags.precis == 0 && flags.point == 1)
+		str = "";
+	size = ft_strlen(str);
+	count += print_width(flags.width - ((flags.precis > 0 && flags.precis < size) ? flags.precis : size), ' ');
+	count += ft_putstr(str, ((flags.precis > 0 && flags.precis < size) ? flags.precis : size));
+	return (count);
+}
+
+int check_long(unsigned long long number)
+{
+	int count = 0;
+
+	while (number > 0)
+	{
+		count++;
+		number /= 16;
+	}
+	return (count);
+}
+
+char *get_long(unsigned int number)
+{
+	char *str;
+	int size;
+	char *array_hex = "0123456789abcdef";
+
+	size = check_long(number);
+	str = (char *)malloc(sizeof(char) * (size + 1));
+	str[size] = '\0';
+	if (number == 0)
+	{
+		str[0] = '0';
+		return (str);
+	}
+	while (--size > 0)
+	{
+		str[size] = array_hex[number % 16];
+		number /= 16;
+	}
+	if (size == 0)
+		str[size] = array_hex[number % 16];
+	return (str);
+}
+
+int print_xxx(t_list flags, va_list value)
+{
+	int count = 0;
+	int size;
+	char *str;
+	unsigned int number;
+
+	number = va_arg(value, unsigned int);
+	str = get_long(number);
+	if (flags.precis == 0 && flags.point == 1 && number == 0)
+	{
+		free(str);
+		str = "";
+	}
+	size = ft_strlen(str);
+	count += print_width(flags.width - ((flags.precis > size) ? flags.precis : size), ' ');
+	count += print_width(flags.precis - size, '0');
+	count += ft_putstr(str, size);
+	if (!(flags.precis == 0 && flags.point == 1 && number == 0))
+		free(str);
+	return (count);
+}
+
+int 	sort_spec(char c, t_list flags, va_list value)
+{
+	int count = 0;
+
+	if (c == 'd')
+		count += print_int(flags, value);
+	if (c == 's')
+		count += print_str(flags, value);
+	if (c == 'x')
+		count += print_xxx(flags, value);
+	return (count);
+}
+
+int ft_printf(char *str, ...)
+{
+	int count = 0;
+	va_list value;
+	int i = 0;
+	t_list flags;
+
+	va_start(value, str);
+	while (str[i])
+	{
+		if (str[i] == '%')
+		{
+			i++;
+			flags = init_struct(flags);
+			flags = pars_flags(flags, &str[i]);
+			while (is_spec(str[i]) == 0)
+				i++;
+			count += sort_spec(str[i], flags, value);
+		}
 		else
-			printf "${grn}▓${end}" 
-        fi
-        if [ $ACT -gt 12 ];
-			then
-			((ERR++))
-			printf "${red}▓${end}" 
-		else
-			printf "${grn}▓${end}"
-		fi
-        if [ $ACT -gt $WRC ];
-            then
-            WRC=$ACT
-        fi
-        printf " actions: %-4i" $ACT
-        echo ' input: ' $ARG
-done
-printf "\e[30;103m\tWorst case found: %i ${end}" $WRC
-
-if [ $ERR -eq 0 ];
-	then
-	printf "${grn}\tSUCCESS [200/200]${end}\n" $ERR
-else
-	printf "${red}\tFAIL[%i/200] 😱 😱 😱 ${end}\n" $ERR
-fi
-
-printf "\n\e[30;103m\tTesting with 5 params${end}\n" ##############################
-ERR=0
-WRC=0
-for i in range {1..99}
-	do 
-		ARG=`ruby -e "puts (0..4).to_a.shuffle.join(' ')"`
-		RET=`./push_swap $ARG | ./checker $ARG`
-        ACT=`./push_swap $ARG | wc -l`
-		if [ "$RET" != "OK" ];
-			then
-			((ERR++))
-			printf "${red}▓${end}" 
-		else
-			printf "${grn}▓${end}" 
-        fi
-        if [ $ACT -gt 12 ];
-			then
-			((ERR++))
-			printf "${red}▓${end}" 
-		else
-			printf "${grn}▓${end}"
-		fi
-        if [ $ACT -gt $WRC ];
-            then
-            WRC=$ACT
-        fi
-        printf " actions: %-4i" $ACT
-        echo ' input: ' $ARG
-done
-printf "\e[30;103m\tWorst case found: %i ${end}" $WRC
-
-if [ $ERR -eq 0 ];
-	then
-	printf "${grn}\tSUCCESS [200/200]${end}\n" $ERR
-else
-	printf "${red}\tFAIL[%i/200] 😱 😱 😱 ${end}\n" $ERR
-fi
-
-printf "\n\e[30;103m\tTesting in a range from -50 to 49${end}\n" ####################################
-ERR=0
-WRC=0
-for i in range {1..99}
-	do 
-		ARG=`ruby -e "puts (-50..49).to_a.shuffle.join(' ')"`
-		RET=`./push_swap $ARG | ./checker $ARG`
-        ACT=`./push_swap $ARG | wc -l`
-		if [ "$RET" != "OK" ];
-			then
-			((ERR++))
-			printf "${red}▓${end}" 
-		else
-			printf "${grn}▓${end}" 
-        fi
-        if [ $ACT -gt 700 ];
-			then
-			((ERR++))
-			printf "${red}▓${end}" 
-		else
-			printf "${grn}▓${end}"
-		fi
-        if [ $ACT -gt $WRC ];
-            then
-            WRC=$ACT
-        fi
-        printf " actions: %-4i\n" $ACT
-done
-printf "\e[30;103m\tWorst case found: %i ${end}" $WRC
-
-if [ $ERR -eq 0 ];
-	then
-	printf "${grn}\tSUCCESS [200/200]${end}\n" $ERR
-else
-	printf "${red}\tFAIL[%i/200] 😱 😱 😱 ${end}\n" $ERR
-fi
-
-
-printf "\n\e[30;103m\tTesting a in a range from -99 to 0${end}\n" ######################################
-ERR=0
-WRC=0
-for i in range {1..99}
-	do 
-		ARG=`ruby -e "puts (-99..0).to_a.shuffle.join(' ')"`
-		RET=`./push_swap $ARG | ./checker $ARG`
-        ACT=`./push_swap $ARG | wc -l`
-		if [ "$RET" != "OK" ];
-			then
-			((ERR++))
-			printf "${red}▓${end}" 
-		else
-			printf "${grn}▓${end}" 
-        fi
-        if [ $ACT -gt 700 ];
-			then
-			((ERR++))
-			printf "${red}▓${end}" 
-		else
-			printf "${grn}▓${end}"
-		fi
-        if [ $ACT -gt $WRC ];
-            then
-            WRC=$ACT
-        fi
-        printf " actions: %-4i\n" $ACT
-done
-printf "\e[30;103m\tWorst case found: %i ${end}" $WRC
-
-if [ $ERR -eq 0 ];
-	then
-	printf "${grn}\tSUCCESS [200/200]${end}\n" $ERR
-else
-	printf "${red}\tFAIL[%i/200] 😱 😱 😱 ${end}\n" $ERR
-fi
-
-printf "\n\e[30;103m\tTesting a in a range from 0 to 99${end}\n" ########################################
-ERR=0
-WRC=0
-for i in range {1..99}
-	do 
-		ARG=`ruby -e "puts (0..99).to_a.shuffle.join(' ')"`
-		RET=`./push_swap $ARG | ./checker $ARG`
-        ACT=`./push_swap $ARG | wc -l`
-		if [ "$RET" != "OK" ];
-			then
-			((ERR++))
-			printf "${red}▓${end}" 
-		else
-			printf "${grn}▓${end}" 
-        fi
-        if [ $ACT -gt 700 ];
-			then
-			((ERR++))
-			printf "${red}▓${end}" 
-		else
-			printf "${grn}▓${end}"
-		fi
-        if [ $ACT -gt $WRC ];
-            then
-            WRC=$ACT
-        fi
-        printf " actions: %-4i\n" $ACT
-done
-printf "\e[30;103m\tWorst case found: %i ${end}" $WRC
-
-if [ $ERR -eq 0 ];
-	then
-	printf "${grn}\tSUCCESS [200/200]${end}\n" $ERR
-else
-	printf "${red}\tFAIL[%i/200] 😱 😱 😱 ${end}\n" $ERR
-fi
-
-printf "\n\e[30;103m\tTesting in a range from 0 to 499${end}\n" ########################################
-ERR=0
-for i in range {1..99}
-	do 
-		ARG=`ruby -e "puts (0..499).to_a.shuffle.join(' ')"`
-		RET=`./push_swap $ARG | ./checker $ARG`
-        ACT=`./push_swap $ARG | wc -l`
-		if [ "$RET" != "OK" ];
-			then
-			((ERR++))
-			printf "${red}▓${end}" 
-		else
-			printf "${grn}▓${end}" 
-        fi
-        if [ $ACT -gt 5500 ];
-			then
-			((ERR++))
-			printf "${red}▓${end}" 
-		else
-			printf "${grn}▓${end}"
-		fi
-        if [ $ACT -gt $WRC ];
-            then
-            WRC=$ACT
-        fi
-        printf " actions: %-4i\n" $ACT
-done
-
-if [ $ERR -eq 0 ];
-	then
-	printf "${grn}\tSUCCESS [200/200]${end}\n" $ERR
-else
-	printf "${red}\tFAIL[%i/200] 😱 😱 😱 ${end}\n" $ERR
-fi
+		{
+			write(1, &str[i], 1);
+			count++;
+		}
+		i++;
+	}
+	va_end(value);
+	return (count);
+}
